4aa4c268b1ac88243018cffc2f6947cdfa0ca505
Paweł Bieniek <pawel@pawel.local>
2018-12-18 12:00:31 +0100

    wywołanie accounting.controller

--- controller/root_controller.py
+++ controller/root_controller.py
@@ -10,27 +10,28 @@ from controller import common
 
 
 def run():
-    options = ["Store manager",
-               "Human resources manager",
-               "Inventory manager",
-               "Accounting manager",
-               "Sales manager",
-               "Customer Relationship Management (CRM)"]
+    # options = ["Store manager",
+    #            "Human resources manager",
+    #            "Inventory manager",
+    #            "Accounting manager",
+    #            "Sales manager",
+    #            "Customer Relationship Management (CRM)"]
 
-    choice = None
-    while choice != "0":
-        choice = terminal_view.get_choice(options)
-        if choice == "1":
-            store_controller.run()
-        elif choice == "2":
-            hr_controller.run()
-        elif choice == "3":
-            inventory_controller.run()
-        elif choice == "4":
-            accounting_controller.run()
-        elif choice == "5":
-            sales_controller.run()
-        elif choice == "6":
-            crm_controller.run()
-        else:
-            terminal_view.print_error_message("There is no such choice.")
+    # choice = None
+    # while choice != "0":
+    #     choice = terminal_view.get_choice(options)
+    #     if choice == "1":
+    #         store_controller.run()
+    #     elif choice == "2":
+    #         hr_controller.run()
+    #     elif choice == "3":
+    #         inventory_controller.run()
+    #     elif choice == "4":
+    #         accounting_controller.run()
+    #     elif choice == "5":
+    #         sales_controller.run()
+    #     elif choice == "6":
+    #         crm_controller.run()
+    #     else:
+    #         terminal_view.print_error_message("There is no such choice.")
+    accounting_controller.run()
